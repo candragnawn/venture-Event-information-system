621ed254666fff5091558220ddbd923ce8917f16
I Kadek Candra Gunawan <kadekcandra1630@gmail.com>
2026-04-04 22:08:52 +0800

    fixing custome path for prisma client

---
+++ venture-backend/prisma/migrations/20260404134557_change_event_to_camel_case/migration.sql
@@ -0,0 +1,68 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `event_id` on the `Favourite` table. All the data in the column will be lost.
+  - You are about to drop the column `event_id` on the `Reviews` table. All the data in the column will be lost.
+  - You are about to drop the column `event_id` on the `Ticket` table. All the data in the column will be lost.
+  - You are about to drop the `event` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `Event_id` to the `Favourite` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `Event_id` to the `Reviews` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `Event_id` to the `Ticket` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Favourite" DROP CONSTRAINT "Favourite_event_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Reviews" DROP CONSTRAINT "Reviews_event_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Ticket" DROP CONSTRAINT "Ticket_event_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "event" DROP CONSTRAINT "event_organizer_id_fkey";
+
+-- AlterTable
+ALTER TABLE "Favourite" DROP COLUMN "event_id",
+ADD COLUMN     "Event_id" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Reviews" DROP COLUMN "event_id",
+ADD COLUMN     "Event_id" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Ticket" DROP COLUMN "event_id",
+ADD COLUMN     "Event_id" INTEGER NOT NULL;
+
+-- DropTable
+DROP TABLE "event";
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" "Category" NOT NULL,
+    "location" TEXT NOT NULL,
+    "Event_date" TIMESTAMP(3) NOT NULL,
+    "Event_time" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "quota" INTEGER NOT NULL,
+    "image_url" TEXT,
+    "organizer_id" INTEGER NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_organizer_id_fkey" FOREIGN KEY ("organizer_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ticket" ADD CONSTRAINT "Ticket_Event_id_fkey" FOREIGN KEY ("Event_id") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reviews" ADD CONSTRAINT "Reviews_Event_id_fkey" FOREIGN KEY ("Event_id") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Favourite" ADD CONSTRAINT "Favourite_Event_id_fkey" FOREIGN KEY ("Event_id") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- venture-backend/src/event/event.service.ts
+++ venture-backend/src/event/event.service.ts
@@ -2,31 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, Event } from '../../generated/prisma';
 
 @Injectable()
 export class EventService {
   constructor(private prisma: PrismaService) {}
 
-
-  
   async createEvent(data: Prisma.EventCreateInput): Promise<Event> {
-    return 'This action adds a new event';
+    return this.prisma.event.create({
+      data,
+    });
   }
 
-  findAll() {
-    return `This action returns all event`;
-  }
+  // findAll() {
+  //   return `This action returns all event`;
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} event`;
+  // }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
-  }
+  // update(id: number, updateEventDto: UpdateEventDto) {
+  //   return `This action updates a #${id} event`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} event`;
+  // }
 }
