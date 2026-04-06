2a3535fb24d0301a47000c9385bb83a0f0355707
I Kadek Candra Gunawan <kadekcandra1630@gmail.com>
2026-04-06 13:02:33 +0800

    generate all resource

--- venture-backend/src/app.module.ts
+++ venture-backend/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { EventModule } from './event/event.module';
+import { EventModule } from './events/event.module';
+import { UserModule } from './user/user.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { FavouriteModule } from './favourite/favourite.module';
 
 @Module({
-  imports: [PrismaModule, EventModule],
+  imports: [PrismaModule, EventModule, UserModule, TicketsModule, OrdersModule, PaymentsModule, ReviewsModule, FavouriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
